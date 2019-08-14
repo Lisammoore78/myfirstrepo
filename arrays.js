eb643d8d78dc9603e204ff5f743a62ab825ee317
lisammore78 <lisamoore78@mail.com>
2019-08-14 13:45:17 +0100

    second commit

---
+++ arrays.js
@@ -0,0 +1,54 @@
+let coffeeOrder =[
+    "alex - coffee",
+    "ben - tea",
+    "charlie - choc"
+];
+console.log(coffeeOrder);
+
+let songList =[
+    "abba - 1",
+    "abba - 2",
+    "abba - 3"
+];
+console.log(songList);
+
+
+
+console.log (coffeeOrder[2]);
+
+coffeeOrder[1] = "ann - vanilla latte";
+console.log (coffeeOrder[2]);
+
+console.log(coffeeOrder.length);
+
+coffeeOrder.push("donna - expresso");
+
+
+
+let websites = [
+    "facebook - 1",
+    "ebay - 2",
+    "linkin - 3"
+];
+console.log(websites);
+
+
+console.log(websites.length);
+ 
+websites.push( "twitter - 4")
+console.log(websites)
+websites.push("insta - 5")
+console.log(websites)
+
+websites.pop()
+
+console.log(websites)
+
+var fruits = ['apple ', 'pear']
+console.log (fruits.length);
+
+fruits.push("orange")
+console.log(fruits)
+fruits.shift('pear')
+console.log(fruits)
+
